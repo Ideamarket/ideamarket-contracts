2a66e545abe763fa84dd3f02611452d3d404c9ad
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-07-21 19:07:27 +0200

    Add cToken interface

---
+++ contracts/compound/ICToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.9;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @title ICToken
+ * @author Alexander Schlindwein
+ *
+ * @dev A simplified interface for Compound's cToken
+ */
+interface ICToken is IERC20 {
+    function exchangeRateStored() external view returns (uint);
+    function accrueInterest() external returns (uint);
+    function mint(uint mintAmount) external returns (uint);
+    function redeem(uint redeemTokens) external returns (uint);
+    function redeemUnderlying(uint redeemAmount) external returns (uint);
+}
