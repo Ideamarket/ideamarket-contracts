f79377cac1486929b21bb42446afc47283025a56
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-10-05 14:25:21 +0200

    Add Timelock contracts

---
+++ contracts/upgradability/DSPauseProxy.sol
@@ -0,0 +1,33 @@
+// Copyright (C) 2019 David Terry <me@xwvvvvwx.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// Updated by Alexander Schlindwein
+
+pragma solidity ^0.6.9;
+
+contract DSPauseProxy {
+    address public _owner;
+    modifier auth { require(msg.sender == _owner, "ds-pause-proxy-unauthorized"); _; }
+    constructor() public { _owner = msg.sender; }
+
+    function exec(address usr, bytes memory fax)
+        public auth
+        returns (bytes memory out)
+    {
+        bool ok;
+        (ok, out) = usr.delegatecall(fax);
+        require(ok, "ds-pause-delegatecall-error");
+    }
+}
