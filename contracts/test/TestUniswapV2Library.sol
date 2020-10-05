44660270b2631e7507f14692193d7a22e2013d8b
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-10-05 11:21:13 +0200

    Tests: Dont use CREATE2 in UniswapV2Library
    This calculated the wrong address for the pair.
    Not entirely sure why - I assume because the contract (UniswapV2Pair)
    has been modified by us.

--- contracts/test/TestUniswapV2Library.sol
+++ contracts/test/TestUniswapV2Library.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.6.9;
 
 import './ITestUniswapV2Pair.sol';
+import './ITestUniswapV2Factory.sol';
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
@@ -16,14 +17,9 @@ library TestUniswapV2Library {
     }
 
     // calculates the CREATE2 address for a pair without making any external calls
-    function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
-        (address token0, address token1) = sortTokens(tokenA, tokenB);
-        pair = address(uint(keccak256(abi.encodePacked(
-                hex'ff',
-                factory,
-                keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
-            ))));
+    // NOTE: THIS FUNCTION HAS BEEN MODIFIED. SEE COMMIT MSG.
+    function pairFor(address factory, address tokenA, address tokenB) internal view returns (address pair) {
+        return ITestUniswapV2Factory(factory).getPair(tokenA, tokenB);
     }
 
     // fetches and sorts the reserves for a pair
