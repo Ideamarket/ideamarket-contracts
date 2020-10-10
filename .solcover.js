55b1642f2cd27a5a460a54acdd71695026d0438a
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-10-10 15:07:53 +0200

    Install solidity coverage

---
+++ .solcover.js
@@ -0,0 +1,20 @@
+module.exports = {
+    skipFiles: [
+        'Migrations.sol',
+        'test/ITestUniswapV2Callee.sol',
+        'test/ITestUniswapV2ERC20.sol',
+        'test/ITestUniswapV2Factory.sol',
+        'test/ITestUniswapV2Pair.sol',
+        'test/Math.sol',
+        'test/TestCDai.sol',
+        'test/TestERC20.sol',
+        'test/TestTransferHelper.sol',
+        'test/TestUniswapV2ERC20.sol',
+        'test/TestUniswapV2Factory.sol',
+        'test/TestUniswapV2Library.sol',
+        'test/TestUniswapV2Pair.sol',
+        'test/TestUniswapV2Router02.sol',
+        'test/TestWETH.sol',
+        'test/UQ112x112.sol'
+    ]
+}
