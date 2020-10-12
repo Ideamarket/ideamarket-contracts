ceec4a64cafc126f8fde9922dc2315cb96d9713b
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-10-12 14:54:46 +0200

    Add IdeaTokenFactory migration

---
+++ migrations/4_deploy_idea_token_factory.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const { externalContractAddresses, saveDeployedAddress, loadDeployedAddress } = require('./shared')
+const { deployProxy } = require('@openzeppelin/truffle-upgrades')
+
+const IdeaTokenFactory = artifacts.require('IdeaTokenFactory')
+
+module.exports = async function(deployer, network, accounts) {
+    let externalAddresses
+
+    if(network == 'kovan') {
+        externalAddresses = externalContractAddresses.kovan
+    } else {
+        return
+    }
+
+    const ideaTokenFactory = await deployProxy(IdeaTokenFactory,
+                                               [
+                                                   externalAddresses.multisig,
+                                                   loadDeployedAddress(network, 'ideaTokenExchange')
+                                               ],
+                                               { deployer })
+
+    saveDeployedAddress(network, 'ideaTokenFactory', ideaTokenFactory.address)
+}
