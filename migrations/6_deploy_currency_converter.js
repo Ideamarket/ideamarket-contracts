89eb59cff3ad3be9ec1ac2f1b8e6c72063b2126f
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-10-12 15:26:24 +0200

    Add CurrencyConverter migration

---
+++ migrations/6_deploy_currency_converter.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const { externalContractAddresses, saveDeployedAddress, loadDeployedAddress } = require('./shared')
+
+const CurrencyConverter = artifacts.require('CurrencyConverter')
+
+module.exports = async function(deployer, network, accounts) {
+    let externalAddresses
+
+    if(network == 'kovan') {
+        externalAddresses = externalContractAddresses.kovan
+    } else {
+        return
+    }
+
+    const currencyConverter = await deployer.deploy(CurrencyConverter,
+                                                    loadDeployedAddress(network, 'ideaTokenExchange'),
+                                                    externalAddresses.dai,
+                                                    externalAddresses.uniswapV2Router02,
+                                                    externalAddresses.weth)
+
+    saveDeployedAddress(network, 'currencyConverter', currencyConverter.address)
+}
