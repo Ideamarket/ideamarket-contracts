2ea170931bd1cd525174339f27d411c839e674d3
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-10-13 15:23:05 +0200

    Add migration for DomainNoSubdomainNameVerifier

---
+++ migrations/12_deploy_domain_no_subdomain_name_verifier.js
@@ -0,0 +1,17 @@
+const { externalContractAddresses, saveDeployedAddress, verifyOnEtherscan } = require('./shared')
+const DomainNoSubdomainNameVerifier = artifacts.require('DomainNoSubdomainNameVerifier')
+
+module.exports = async function(deployer, network, accounts) {
+    let externalAddresses
+
+    if(network == 'kovan') {
+        externalAddresses = externalContractAddresses.kovan
+    } else {
+        return
+    }
+
+    await deployer.deploy(DomainNoSubdomainNameVerifier)
+
+    await verifyOnEtherscan(network, DomainNoSubdomainNameVerifier.address, 'DomainNoSubdomainNameVerifier')
+    saveDeployedAddress(network, 'domainNoSubdomainNameVerifier', DomainNoSubdomainNameVerifier.address)
+}
