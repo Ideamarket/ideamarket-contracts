0557f68beb701dbe7ecb6c0828842ab81b2fa418
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-10-13 10:29:10 +0200

    Add more migrations

---
+++ migrations/6_set_interest_manager_owner.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const { externalContractAddresses, loadDeployedAddress } = require('./shared')
+
+const InterestManagerCompound = artifacts.require('InterestManagerCompound')
+
+module.exports = async function(deployer, network, accounts) {
+    let externalAddresses
+
+    if(network == 'kovan') {
+        externalAddresses = externalContractAddresses.kovan
+    } else {
+        return
+    }
+
+    const interestManagerCompound = await InterestManagerCompound.at(loadDeployedAddress(network, 'interestManager'))
+    await interestManagerCompound.setOwner(loadDeployedAddress(network, 'ideaTokenExchange'))
+}
---
+++ migrations/7_set_idea_token_exchange_factory_address.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const { externalContractAddresses, loadDeployedAddress } = require('./shared')
+
+const IdeaTokenExchange = artifacts.require('IdeaTokenExchange')
+
+module.exports = async function(deployer, network, accounts) {
+    let externalAddresses
+
+    if(network == 'kovan') {
+        externalAddresses = externalContractAddresses.kovan
+    } else {
+        return
+    }
+
+    const ideaTokenExchange = await IdeaTokenExchange.at(loadDeployedAddress(network, 'ideaTokenExchange'))
+    await ideaTokenExchange.setIdeaTokenFactoryAddress(loadDeployedAddress(network, 'ideaTokenFactory'))
+}
