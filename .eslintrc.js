a991fb716aaa9ccd360da36d726275621fafa3b9
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-10-14 14:27:01 +0200

    Install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 12
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
