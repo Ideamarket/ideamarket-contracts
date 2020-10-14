39f707dc5b8a83bb7513e4b9b570d917950ab6f5
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-10-14 14:01:51 +0200

    Add migrations for trading and platform fee spells

---
+++ migrations/15_deploy_set_trading_fee_spell.js
@@ -0,0 +1,17 @@
+const { externalContractAddresses, saveDeployedAddress, verifyOnEtherscan } = require('./shared')
+const SetTradingFeeSpell = artifacts.require('SetTradingFeeSpell')
+
+module.exports = async function(deployer, network, accounts) {
+    let externalAddresses
+
+    if(network == 'kovan') {
+        externalAddresses = externalContractAddresses.kovan
+    } else {
+        return
+    }
+
+    await deployer.deploy(SetTradingFeeSpell)
+
+    await verifyOnEtherscan(network, SetTradingFeeSpell.address, 'SetTradingFeeSpell')
+    saveDeployedAddress(network, 'setTradingFeeSpell', SetTradingFeeSpell.address)
+}
---
+++ migrations/16_deploy_set_platform_fee_spell.js
@@ -0,0 +1,17 @@
+const { externalContractAddresses, saveDeployedAddress, verifyOnEtherscan } = require('./shared')
+const SetPlatformFeeSpell = artifacts.require('SetPlatformFeeSpell')
+
+module.exports = async function(deployer, network, accounts) {
+    let externalAddresses
+
+    if(network == 'kovan') {
+        externalAddresses = externalContractAddresses.kovan
+    } else {
+        return
+    }
+
+    await deployer.deploy(SetPlatformFeeSpell)
+
+    await verifyOnEtherscan(network, SetPlatformFeeSpell.address, 'SetPlatformFeeSpell')
+    saveDeployedAddress(network, 'setPlatformFeeSpell', SetPlatformFeeSpell.address)
+}
