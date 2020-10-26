c591817d6e3a22027c334b52cface21da11602bc
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-10-26 19:11:56 +0100

    Install hardhat web3 plugin

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,26 +1,27 @@
-require("@nomiclabs/hardhat-waffle")
+require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-web3')
 require('dotenv').config()
 
 module.exports = {
-    networks: {
-      kovan: {
-        url: `https://kovan.infura.io/v3/${process.env.INFURA_KEY}`,
-        accounts: [ process.env.KOVAN_PRIVATE_KEY ]
-      }
-    },
-    solidity: {
-      version: '0.6.9',
-      settings: {
-        /*optimizer: {
+	networks: {
+		kovan: {
+			url: `https://kovan.infura.io/v3/${process.env.INFURA_KEY}`,
+			accounts: [ process.env.KOVAN_PRIVATE_KEY ]
+		}
+	},
+	solidity: {
+		version: '0.6.9',
+		settings: {
+			/*optimizer: {
           enabled: true,
           runs: 200
         }*/
-      }
-    },
-    paths: {
-      sources: './contracts',
-      tests: './test/contracts',
-      cache: './cache',
-      artifacts: './build/contracts'
-    }
-  }
+		}
+	},
+	paths: {
+		sources: './contracts',
+		tests: './test/contracts',
+		cache: './cache',
+		artifacts: './build/contracts'
+	}
+}
