085896b2bd91c6bf2abf567f35412c7653bb0533
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-10-26 16:20:08 +0100

    Migrate DSPauseProxy

--- test/contracts/timelock/DSPauseProxy.test.js
+++ test/contracts/timelock/DSPauseProxy.test.js
@@ -1,12 +1,13 @@
-const { expectRevert, time } = require('@openzeppelin/test-helpers')
-const DSPause = artifacts.require('DSPause')
-const DSPauseProxy = artifacts.require('DSPauseProxy')
-// We use the AddMarketSpell to test the DSPause
-const AddMarketSpell = artifacts.require('AddMarketSpell')
+const { time } = require('@openzeppelin/test-helpers')
+const { expect } = require('chai')
+const { BigNumber } = require('ethers')
+const { ethers } = require('hardhat')
 
-const BN = web3.utils.BN
+describe('timelock/DSPauseProxy', () => {
 
-contract('timelock/DSPauseProxy', async accounts => {
+	let DSPause
+	let DSPauseProxy
+	let AddMarketSpell
 
 	let dsPause
 	let dsPauseProxy
@@ -14,31 +15,39 @@ contract('timelock/DSPauseProxy', async accounts => {
 
 	const delay = 86400
 	const zeroAddress = '0x0000000000000000000000000000000000000000'
-	const adminAccount = accounts[0]
-    
+	let adminAccount
+	
+	before(async () => {
+		const accounts = await ethers.getSigners()
+		adminAccount = accounts[0]
+
+		DSPause = await ethers.getContractFactory('DSPause')
+		DSPauseProxy = await ethers.getContractFactory('DSPauseProxy')
+		AddMarketSpell = await ethers.getContractFactory('AddMarketSpell')
+	})
+
 	beforeEach(async () => {
-		dsPause = await DSPause.new(delay, adminAccount)
-		dsPauseProxy = await DSPauseProxy.at(await dsPause._proxy())
-		spell = await AddMarketSpell.new()
+		dsPause = await DSPause.deploy(delay, adminAccount.address)
+		await dsPause.deployed()
+
+		dsPauseProxy = new ethers.Contract(await dsPause._proxy(), DSPauseProxy.interface, DSPauseProxy.signer)
+
+		spell = await AddMarketSpell.deploy()
+		await spell.deployed()
 	})
 
 	it('fail unauthorized exec', async () => {
-		await expectRevert(dsPauseProxy.exec(zeroAddress, []), 'ds-pause-proxy-unauthorized')
+		await expect(dsPauseProxy.exec(zeroAddress, [])).to.be.revertedWith('ds-pause-proxy-unauthorized')
 	})
 
 	it('fail delegatecall error', async () => {
-		const eta = new BN((parseInt(await time.latest()) + delay + 100).toString())
+		const eta = BigNumber.from((parseInt(await time.latest()) + delay + 100).toString())
 		const tag = await dsPause.soul(spell.address)
-    
-		const fax = spell.contract.methods.execute(zeroAddress, 'SOME_MARKET', zeroAddress, // zero for factory addr
-			'1', '1',
-			'0', '0').encodeABI()
+	
+		const fax = spell.interface.encodeFunctionData('execute', [zeroAddress, 'SOME_MARKET', zeroAddress, '1', '1', '0', '0'])
 
 		await dsPause.plot(spell.address, tag, fax, eta)
-		await time.increaseTo(eta.add(new BN('1')))
-		await expectRevert(
-			dsPause.exec(spell.address, tag, fax, eta),
-			'ds-pause-delegatecall-error'
-		)
+		await time.increaseTo(eta.add(BigNumber.from('1')).toString())
+		await expect(dsPause.exec(spell.address, tag, fax, eta)).to.be.revertedWith('ds-pause-delegatecall-error')
 	})
 })
