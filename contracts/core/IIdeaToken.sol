90028754d1204e553b0617a370942d81b5d0f50b
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-11-21 20:50:40 +0100

    IIdeaToken: Improve natspec

--- contracts/core/IIdeaToken.sol
+++ contracts/core/IIdeaToken.sol
@@ -6,8 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /**
  * @title IIdeaToken
  * @author Alexander Schlindwein
- *
- * @dev Interface for IdeaTokens. Extends ERC20 with mint and burn.
  */
 interface IIdeaToken is IERC20 {
     function mint(address account, uint256 amount) external;
