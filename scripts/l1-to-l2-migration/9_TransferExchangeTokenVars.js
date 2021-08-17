c1019109acf2272e87da58e734e91eb9ce5b1e52
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2021-08-17 10:41:17 +0000

    feat: wait for l2 tx

--- scripts/l1-to-l2-migration/9_TransferExchangeTokenVars.js
+++ scripts/l1-to-l2-migration/9_TransferExchangeTokenVars.js
@@ -1,5 +1,6 @@
 const { BigNumber } = require('ethers')
 const { ethers } = require('hardhat')
+const { Bridge } = require('arb-ts')
 const { read, loadDeployedAddress } = require('../shared')
 const config = require('./config')
 
@@ -9,24 +10,36 @@ async function main() {
 	const deployerAccount = (await ethers.getSigners())[0]
 	const deployerAddress = deployerAccount.address
 
-	let networkName = (await ethers.provider.getNetwork()).name
-	if (networkName === 'rinkeby') {
-		networkName = 'test-avm-l1'
+	let l1NetworkName = (await ethers.provider.getNetwork()).name
+	let l2NetworkName
+
+	if (l1NetworkName === 'rinkeby') {
+		l1NetworkName = 'test-avm-l1'
+		l2NetworkName = 'test-avm-l2'
 	} else {
-		throw 'cannot work with network: ' + networkName
+		throw 'cannot work with network: ' + l1NetworkName
 	}
 
-	const deploymentParams = config.deploymentParams[networkName]
-	const ideaTokenFactoryAddress = loadDeployedAddress(networkName, 'ideaTokenFactory')
+	const deploymentParams = config.deploymentParams[l1NetworkName]
+
+	const l1EthParams = config.ethParams[l1NetworkName]
+	const l2EthParams = config.ethParams[l2NetworkName]
+	const l1Provider = new ethers.providers.JsonRpcProvider(l1EthParams.rpcUrl)
+	const l2Provider = new ethers.providers.JsonRpcProvider(l2EthParams.rpcUrl)
+	const l1Wallet = new ethers.Wallet(l1EthParams.privateKey, l1Provider)
+	const l2Wallet = new ethers.Wallet(l2EthParams.privateKey, l2Provider)
+	const bridge = await Bridge.init(l1Wallet, l2Wallet)
+
+	const ideaTokenFactoryAddress = loadDeployedAddress(l1NetworkName, 'ideaTokenFactory')
 	const ideaTokenFactory = new ethers.Contract(
 		ideaTokenFactoryAddress,
 		(await ethers.getContractFactory('IdeaTokenFactory')).interface,
 		deployerAccount
 	)
 	const numMarkets = await ideaTokenFactory.getNumMarkets()
-	const ideaTokenExchangeStateTransferAddress = loadDeployedAddress(networkName, 'ideaTokenExchange')
+	const ideaTokenExchangeStateTransferAddress = loadDeployedAddress(l1NetworkName, 'ideaTokenExchange')
 
-	console.log('Network', networkName)
+	console.log('Network', l1NetworkName)
 	console.log('Deployer ', deployerAddress)
 	console.log('Gas Price', deploymentParams.gasPrice)
 	console.log('')
@@ -100,6 +113,16 @@ async function main() {
 				}
 			)
 			await tx.wait()
+			console.log('Transaction confirmed on L1')
+
+			const receipt = await ethers.provider.getTransactionReceipt(tx.hash)
+			const seqNum = (await bridge.getInboxSeqNumFromContractTransaction(receipt))[0]
+			console.log('Awaiting L2 tx. SeqNum', seqNum.toString())
+			const result = await bridge.waitForRetryableReceipt(seqNum)
+			if(result.status !== 1) {
+				console.log(result)
+				throw Error('L2 tx failed')
+			}
 		}
 	}
 }
--- scripts/l1-to-l2-migration/config.js
+++ scripts/l1-to-l2-migration/config.js
@@ -1,24 +1,25 @@
 const { BigNumber } = require('ethers')
+require('dotenv').config()
 
 module.exports = {
 	deploymentParams: {
 		'test-avm-l1': {
 			gasPrice: 10000000000, // 10 gwei
 
-			gasLimitInterestManagerStateTransfer: BigNumber.from('1500000'), // 1.5MM
-			maxSubmissionCostInterestManagerStateTransfer: BigNumber.from('1000000000000000'), // 0.001 ETH
+			gasLimitInterestManagerStateTransfer: BigNumber.from('3000000'), // 1.5MM
+			maxSubmissionCostInterestManagerStateTransfer: BigNumber.from('2000000000000000'), // 0.002 ETH
 			l2GasPriceBidInterestManagerStateTransfer: BigNumber.from('1000000000'), // 1 gwei
 
-			gasLimitExchangeStaticVarsTransfer: BigNumber.from('1000000'), // 1MM
-			maxSubmissionCostExchangeStaticVarsTransfer: BigNumber.from('1000000000000000'), // 0.001 ETH
+			gasLimitExchangeStaticVarsTransfer: BigNumber.from('2000000'), // 2MM
+			maxSubmissionCostExchangeStaticVarsTransfer: BigNumber.from('2000000000000000'), // 0.002 ETH
 			l2GasPriceBidExchangeStaticVarsTransfer: BigNumber.from('1000000000'), // 1 gwei
 
-			gasLimitExchangePlatformVarsTransfer: BigNumber.from('1000000'), // 1MM
-			maxSubmissionCostExchangePlatformVarsTransfer: BigNumber.from('1000000000000000'), // 0.001 ETH
+			gasLimitExchangePlatformVarsTransfer: BigNumber.from('2000000'), // 2MM
+			maxSubmissionCostExchangePlatformVarsTransfer: BigNumber.from('2000000000000000'), // 0.002 ETH
 			l2GasPriceBidExchangePlatformVarsTransfer: BigNumber.from('1000000000'), // 1 gwei
 
 			gasLimitExchangeTokensVarsTransferPerToken: BigNumber.from('500000'), // 500K
-			maxSubmissionCostExchangeTokenVarsTransferPerToken: BigNumber.from('1000000000000000'), // 0.001 ETH
+			maxSubmissionCostExchangeTokenVarsTransferPerToken: BigNumber.from('2000000000000000'), // 0.002 ETH
 			l2GasPriceBidExchangeTokenVarsTransferPerToken: BigNumber.from('1000000000'), // 1 gwei
 		},
 		'test-avm-l2': {
@@ -61,4 +62,14 @@ module.exports = {
 			uniswapV2Router02: '0x0000000000000000000000000000000000000001',
 		},
 	},
+	ethParams: {
+		'test-avm-l1': {
+			rpcUrl: process.env.RINKEBY_RPC,
+			privateKey: process.env.RINKEBY_PRIVATE_KEY,
+		},
+		'test-avm-l2': {
+			rpcUrl: process.env.RINKEBY_AVM_RPC,
+			privateKey: process.env.RINKEBY_AVM_PRIVATE_KEY,
+		},
+	}
 }
